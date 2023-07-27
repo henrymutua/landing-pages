ce6999024a449105407739b1cbd882cd6f9ae479
henrymutua <mutuahenry116@gmail.com>
2023-07-27 16:18:06 +0300

    changed dimensions

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from './SocialMedia.module.css'
 
 const SocialMedia = () => {
   return (
-    <div>SocialMedia</div>
+    <div className={styles.socialMedia}>
+      <p>60 minute read</p>
+      <div className={styles.socialMediaIcons}>
+        <componentOne/>
+        <componentTwo/>
+        <componentThree/>
+        <componentFour/>
+        <componentFive/>
+        <componentSix/>
+      </div>
+    </div>
   )
 }
 
